3b18380ff2d58540f5d4411b5761c3670645bbd3
msthill <micah.sthill123@gmail.com>
2025-07-08 15:11:44 -0500

    Created window, along with a class for the game, init and run function

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import sys
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("JumpSolve")
+        self.screen = pygame.display.set_mode((800, 600))
+
+        self.clock = pygame.time.Clock()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60) 
+
+Game().run()
