742441144469d4305a9657eb1cdce1be2e8957f9
VictorFagbohun <ofagbohun62@gmail.com>
2025-07-21 20:58:26 -0500

    question code finished

--- question.py
+++ question.py
@@ -17,98 +17,103 @@ word_font = pygame.font.Font(None,50)
 clock = pygame.time.Clock()
 points = 0
 
-
-def draw_text(text,font,x,y):
-    img = font.render(text,True,"white")
-    screen.blit(img,(x,y))
-
-def random_selection(question):
-    global png
-    global img
-    png = random.choice(list(question.keys()))
-    img = pygame.image.load(png)
-    print(png)
-    print(easy_question[png])
-
-random_selection(easy_question)
-img = pygame.image.load(png)
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                if easy_question[png] == "A":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-                    
-                    
-            elif event.key == pygame.K_b:
-                if easy_question[png] == "B":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-            elif event.key == pygame.K_c:
-                print("C")
-                if easy_question[png] == "C":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-            elif event.key == pygame.K_d:
-                if easy_question[png] == "D":
-                    print("Correct Answer")
-                    points +=1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-            elif event.key == pygame.K_f:
-                if easy_question[png] == "F":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-                
-            elif event.key == pygame.K_g:
-                if easy_question[png] == "G":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-            elif event.key == pygame.K_h:
-                print("C")
-                if easy_question[png] == "H":
-                    print("Correct Answer")
-                    points += 1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-            elif event.key == pygame.K_j:
-                if easy_question[png] == "J":
-                    print("Correct Answer")
-                    points +=1
-                    del easy_question[png]
-                    random_selection(easy_question)
-                    img = pygame.image.load(png)
-                    
-    if points == 3:
-        print("Congratulations")
-        break
-  
-
-    screen.blit(img,[150,0])
+def questions(difficulty):
     
-    pygame.display.update()
-    clock.tick(60)
+    if difficulty == "Easy":
+        question = easy_question
+    elif difficulty == "Medium":
+        question = medium_question
+    elif difficulty == "Hard":
+        question = hard_question
+        
+    def draw_text(text,font,x,y):
+        img = font.render(text,True,"white")
+        screen.blit(img,(x,y))
+
+    def random_selection(question):
+        global png
+        global img
+        png = random.choice(list(question.keys()))
+        img = pygame.image.load(png)
+
+
+    random_selection(question)
+    img = pygame.image.load(png)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    if easy_question[png] == "A":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png) 
+                elif event.key == pygame.K_b:
+                    if easy_question[png] == "B":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                elif event.key == pygame.K_c:
+                    print("C")
+                    if easy_question[png] == "C":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                elif event.key == pygame.K_d:
+                    if easy_question[png] == "D":
+                        print("Correct Answer")
+                        points +=1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                elif event.key == pygame.K_f:
+                    if easy_question[png] == "F":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                    
+                elif event.key == pygame.K_g:
+                    if easy_question[png] == "G":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                elif event.key == pygame.K_h:
+                    print("C")
+                    if easy_question[png] == "H":
+                        print("Correct Answer")
+                        points += 1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                elif event.key == pygame.K_j:
+                    if easy_question[png] == "J":
+                        print("Correct Answer")
+                        points +=1
+                        del easy_question[png]
+                        random_selection(question)
+                        img = pygame.image.load(png)
+                        
+        if points == 3:
+            print("Congratulations")
+            break
+    
+
+        screen.blit(img,[150,0])
+        
+        pygame.display.update()
+        clock.tick(60)
     
